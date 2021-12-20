a38f9a66a55b9dc2141ba2deae67498dd41a7c7f
aaron <aaron@classting.com>
2021-12-20 19:28:30 +0900

    chore(Mantine): MantineUI 추가 및 추가 lint 설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MantineProvider } from '@mantine/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<MantineProvider>
+    <App />
+    </MantineProvider>,
+     document.getElementById('root')
+     );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
