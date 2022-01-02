0047ac0d98adcd1114110feb03798750d9498d16
aaron <aaron@classting.com>
2022-01-02 20:25:23 +0900

    feat(FFmpeg): ffmpeg module 추가 및 파일 변환 함수 추가
    - 파일 변환 기능 실행하기엔 아직 부족

--- src/components/FileDropzone/FileDropzone.tsx
+++ src/components/FileDropzone/FileDropzone.tsx
@@ -1,9 +1,11 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 
-import { Text, Group, useMantineTheme } from '@mantine/core'
+import { Text, Group, useMantineTheme, Button } from '@mantine/core'
 import { Dropzone } from '@mantine/dropzone';
 
 import { ImageIcon, UploadIcon, CrossCircledIcon } from '@modulz/radix-icons';
+
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 type ImageUploadIconProps = {
   status: any;
@@ -36,8 +38,32 @@ const getIconColor = (status: any, theme: any) => {
 const FileDropzone = (): ReactElement => {
   const theme = useMantineTheme()
 
+  const [droppedFile, setDroppedFile] = useState<File | null>(null)
+  const [resultFile, setResultFile] = useState<string | null>(null)
+  const [statusMessage, setStatueMessage] = useState<string>('')
+
+  const ffmpeg = createFFmpeg({
+    log: true,
+  });
+
+  const doTranscode = async () => {
+    setStatueMessage('Loading ffmpeg-core.js');
+    await ffmpeg.load();
+    setStatueMessage('Start transcoding');
+    ffmpeg.FS('writeFile', 'test.avi', await fetchFile('/flame.avi'));
+    await ffmpeg.run('-i', 'test.avi', 'test.mp4');
+    setStatueMessage('Complete transcoding');
+    const data = ffmpeg.FS('readFile', 'test.mp4');
+    setResultFile(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
+  };
+
+  useEffect(() => {
+    console.log(droppedFile)
+  }, [droppedFile])
+
   return (
-    <Dropzone onDrop={console.log}>
+    <>
+    <Dropzone onDrop={(files) => setDroppedFile(files[0])}>
       {(status) => (
         <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
           <ImageUploadIcon
@@ -56,6 +82,8 @@ const FileDropzone = (): ReactElement => {
         </Group>
       )}
     </Dropzone>
+    <Button onClick={doTranscode}>translate</Button>
+    </>
   )
 }
 
